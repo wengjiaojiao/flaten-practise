96335f454c160c8d4e14dc15e1c057acf3f7b0b4
wengjiaojiao <419909058@qq.com>
2016-03-23 17:13:55 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,17 +1,16 @@
 'use strict';
 
 function double_to_one(collection) {
- var all = [];
- +  for(var i= 0; i < collection.length; i++) {
- +      if(collection[i].length == undefined) {
- +          all.push(collection[i]);
- +      }
- +      for(var j = 0; j < collection[i].length; j++) {
- +          all.push(collection[i][j]);
- +      }
- +  }
- +  return all;
- +}
+  var all = [];
+  for(var i= 0; i < collection.length; i++) {
+      if(collection[i].length == undefined) {
+          all.push(collection[i]);
+      }
+      for(var j = 0; j < collection[i].length; j++) {
+          all.push(collection[i][j]);
+      }
+  }
+  return all;
 }
 
 module.exports = double_to_one;
