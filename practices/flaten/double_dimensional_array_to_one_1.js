648bf3c8756c061f871854508c1db89a8c37cdc4
wengjiaojiao <419909058@qq.com>
2016-03-23 17:08:30 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,16 +1,7 @@
 'use strict';
 
 function double_to_one(collection) {
-  var all = [];
-  for(var i= 0; i < collection.length; i++) {
-      if(collection[i].length == undefined) {
-          all.push(collection[i]);
-      }
-      for(var j = 0; j < collection[i].length; j++) {
-          all.push(collection[i][j]);
-      }
-  }
-  return all;
+ return 'adsfa';
 }
 
 module.exports = double_to_one;
